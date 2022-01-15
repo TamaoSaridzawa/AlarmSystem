aba48b1b8959c3f55096206c08dd1d63a53fe147
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2022-01-15 10:43:23 +0300

    Привел в порядок стиль кода

--- Assets/Scripts/IsOpenDoor.cs
+++ Assets/Scripts/IsOpenDoor.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 public class IsOpenDoor : MonoBehaviour
 {
     [SerializeField] private Camera _playerCamera;
+
     private RaycastHit _hit;
     private Ray _ray;
 
     private void Update()
     {
-        //Ray ray = new Ray(transform.position, transform.forward);
-        //Debug.DrawRay(transform.position, transform.forward, Color.red);
-
         _ray = _playerCamera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
 
         if (Physics.Raycast(_ray, out _hit))
         {
             if (_hit.transform.GetComponent<Door>())
             {
-                Debug.Log("Дверь найдена");
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     _hit.transform.GetComponent<Door>().Open();
                 }
             }
         }
-
-       
     }
 }
