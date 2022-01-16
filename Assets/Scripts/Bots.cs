725fb7b6311aa396561f2b2699e21d0567e26b2a
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2022-01-16 17:05:56 +0300

    Генерация врагов #2 (Напутал со скриптами)

--- Assets/Scripts/Bots.cs
+++ Assets/Scripts/Bots.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BotsInstantiate : MonoBehaviour
+public class Bots : MonoBehaviour
 {
     public GameObject Bot;
 
@@ -10,11 +10,8 @@ public class BotsInstantiate : MonoBehaviour
 
     private Transform[] _points;
 
-    private Door _door;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
         _points = new Transform[_path.childCount];
 
         for (int i = 0; i < _path.childCount; i++)
@@ -25,18 +22,8 @@ public class BotsInstantiate : MonoBehaviour
         StartCoroutine(CreateBots());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private IEnumerator CreateBots()
     {
-        if (true)
-        {
-
-        }
         bool isJob = true;
 
         while (isJob)
@@ -46,9 +33,9 @@ public class BotsInstantiate : MonoBehaviour
                 GameObject newBot = Instantiate(Bot, Vector3.zero, Quaternion.identity);
                 Transform transformNewBot = newBot.GetComponent<Transform>();
                 transformNewBot.position = _points[i].position;
+
                 yield return new WaitForSeconds(2f);
             }
         }
-        
     }
 }
